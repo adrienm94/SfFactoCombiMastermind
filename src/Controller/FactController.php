79cc62324b8ba994df64a3e4c5960edd468d8b81
Adrien Meyrand <adrien.meyrand@etu.umontpellier.fr>
2023-09-21 09:33:13 +0200

    Commit 21/09/2023 (2)

--- src/Controller/FactController.php
+++ src/Controller/FactController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-include("../factorielle.php");
+include("factorielle.php");
 
 class FactController extends AbstractController
 {
