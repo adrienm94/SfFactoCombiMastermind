ac189bb8089265f83fc97c5037bc00483a8488f8
Adrien Meyrand <adrien.meyrand@etu.umontpellier.fr>
2023-09-24 10:27:05 +0200

    Commit 24/09/2023

--- src/Controller/FactController.php
+++ src/Controller/FactController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+use PhpParser\Node\Expr\Cast\Int_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-include("factorielle.php");
+use Symfony\Component\HttpFoundation\Request;
 
 class FactController extends AbstractController
 {
-    #[Route('/fact/{n}', name: 'app_fact')]
-    public function index($n): Response
+
+    #[Route('/', name: 'app_fact')]
+    public function index(Request $request): Response
     {
-        return new Response($n."!"."=".factorielle($n));
+
+    	if ($request->query->get('combi_n') != null && $request->query->get('combi_p') != null) {
+    		$n = intval($request->query->get('combi_n'));
+    		$p = intval($request->query->get('combi_p'));
+    		$r = factorielle($n)/(factorielle($p)*factorielle($n-$p));
+			return $this->render('fact/factocombi.html.twig',[
+    			'n' => $n,
+    			'p' => $p, 
+    			'r' => $r]);
+    	} else if ($request->query->get('fact') != null){
+    		$k = intval($request->query->get('fact'));
+    		$r = factorielle($k);
+			return $this->render('fact/factocombi.html.twig',[
+    			'k' => $k, 
+    			'r' => $r]);
+    	} else {
+			return $this->render('base.html.twig',[]);
+    	}
+    	
+
+        
     }
 
     #[Route('/combi/{n}/{p}', name: 'app_combi')]
@@ -22,4 +43,22 @@ class FactController extends AbstractController
         $combinaisons = factorielle($n)/(factorielle($p)*factorielle($n-$p));
         return new Response("Nombre de combinaisons de n éléments dans p places : ".$combinaisons);
     }
+
+    #[Route('/fact/{n}')]
+    public function fact($n): Response 
+    {
+    	return new Response($n."!=".factorielle($n));
+    }
 }
+
+function factorielle($n){
+	 	$res = 1;
+	 	if ($n==0) {
+	 		return $res;
+	 	} else {
+	 		for ($i=1; $i < $n+1; $i++) { 
+	 			$res = $res*$i;
+	 		}
+	 	}
+	 	return $res;
+	 }
